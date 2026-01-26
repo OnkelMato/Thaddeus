a31ea012574f6e5f3bbfe128bcbd86ad56e282b8
Thomas Ley <thomas-ley@outlook.de>
2026-01-26 18:25:58 +0100

    more flexible date and time parser -takes current month/year or hour only in case it is partial.

---
+++ OnkelMato.Thaddeus.Telegram.Tests/BotMessageToRequestConverterTests.cs
@@ -0,0 +1,142 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using OnkelMato.Thaddeus.Telegram.Requests;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace OnkelMato.Thaddeus.Telegram.Tests
+{
+    [TestFixture]
+    public class BotMessageToRequestConverterTests
+    {
+        [Test(Description = "Request termine")]
+        public void ConvertFullTermineToMessage()
+        {
+            var message = new Message()
+            {
+                Text = "Termine 15.8.2024",
+                Chat = new Chat { Id = 12345 }
+            };
+            var sut = new BotMessageToRequestConverter();
+            var actual = sut.Convert(message, UpdateType.Message);
+            actual.Should().BeOfType<GetAppointmentsRequest>();
+            var req = (actual as GetAppointmentsRequest)!;
+            req.Date.Should().Be(new DateOnly(2024, 8, 15));
+        }
+
+        [Test(Description = "Request termine")]
+        public void ConvertNoYearTermineToMessage()
+        {
+            var now = DateTime.Now;
+            var message = new Message()
+            {
+                Text = "Termine 15.8.",
+                Chat = new Chat { Id = 12345 }
+            };
+            var sut = new BotMessageToRequestConverter();
+            var actual = sut.Convert(message, UpdateType.Message);
+            actual.Should().BeOfType<GetAppointmentsRequest>();
+            var req = (actual as GetAppointmentsRequest)!;
+            req.Date.Should().Be(new DateOnly(now.Year, 8, 15));
+        }
+
+        [Test(Description = "Request termine")]
+        public void ConvertNoMonthTermineToMessage()
+        {
+            var now = DateTime.Now;
+            var message = new Message()
+            {
+                Text = "Termine 15.",
+                Chat = new Chat { Id = 12345 }
+            };
+            var sut = new BotMessageToRequestConverter();
+            var actual = sut.Convert(message, UpdateType.Message);
+            actual.Should().BeOfType<GetAppointmentsRequest>();
+            var req = (actual as GetAppointmentsRequest)!;
+            req.Date.Should().Be(new DateOnly(now.Year, now.Month, 15));
+        }
+
+        [Test]
+        public void ConvertFullTerminToMessage()
+        {
+            var message = new Message()
+            {
+                Text = "Termin 15.8.2024 14:30 Meeting with team",
+                Chat = new Chat { Id = 12345 }
+            };
+
+            var sut = new BotMessageToRequestConverter();
+
+            var actual = sut.Convert(message, UpdateType.Message);
+
+            actual.Should().BeOfType<AddAppointmentRequest>();
+            var req = actual as AddAppointmentRequest;
+            req.Appointment.Start.Should().Be(new DateTime(2024, 8, 15, 14, 30, 0));
+            req.Appointment.End.Should().Be(new DateTime(2024, 8, 15, 15, 0, 0));
+            req.Appointment.Title.Should().Be("Meeting with team");
+        }
+
+        [Test]
+        public void ConvertNoYearTerminToMessage()
+        {
+            var now = DateTime.Now;
+            var message = new Message()
+            {
+                Text = "Termin 15.8. 14:30 Meeting with team",
+                Chat = new Chat { Id = 12345 }
+            };
+
+            var sut = new BotMessageToRequestConverter();
+
+            var actual = sut.Convert(message, UpdateType.Message);
+
+            actual.Should().BeOfType<AddAppointmentRequest>();
+            var req = (actual as AddAppointmentRequest)!;
+            req.Appointment.Start.Should().Be(new DateTime(now.Year, 8, 15, 14, 30, 0));
+            req.Appointment.End.Should().Be(new DateTime(now.Year, 8, 15, 15, 0, 0));
+            req.Appointment.Title.Should().Be("Meeting with team");
+        }
+
+        [Test]
+        public void ConvertNoYearMonthTerminToMessage()
+        {
+            var now = DateTime.Now;
+            var message = new Message()
+            {
+                Text = "Termin 15. 14:30 Meeting with team",
+                Chat = new Chat { Id = 12345 }
+            };
+
+            var sut = new BotMessageToRequestConverter();
+
+            var actual = sut.Convert(message, UpdateType.Message);
+
+            actual.Should().BeOfType<AddAppointmentRequest>();
+            var req = (actual as AddAppointmentRequest)!;
+            req.Appointment.Start.Should().Be(new DateTime(now.Year, now.Month, 15, 14, 30, 0));
+            req.Appointment.End.Should().Be(new DateTime(now.Year, now.Month, 15, 15, 0, 0));
+            req.Appointment.Title.Should().Be("Meeting with team");
+        }
+
+        [Test]
+        public void ConvertHourOnlyTerminToMessage()
+        {
+            var message = new Message()
+            {
+                Text = "Termin 15.8.2024 14 Meeting with team",
+                Chat = new Chat { Id = 12345 }
+            };
+
+            var sut = new BotMessageToRequestConverter();
+
+            var actual = sut.Convert(message, UpdateType.Message);
+
+            actual.Should().BeOfType<AddAppointmentRequest>();
+            var req = (actual as AddAppointmentRequest)!;
+            req.Appointment.Start.Should().Be(new DateTime(2024, 8, 15, 14, 00, 0));
+            req.Appointment.End.Should().Be(new DateTime(2024, 8, 15, 14, 30, 0));
+            req.Appointment.Title.Should().Be("Meeting with team");
+        }
+
+    }
+}
--- src/OnkelMato.Thaddeus.Telegram/BotMessageToRequestConverter.cs
+++ src/OnkelMato.Thaddeus.Telegram/BotMessageToRequestConverter.cs
@@ -1,36 +1,91 @@
+using System.Globalization;
+using Microsoft.Extensions.Localization;
 using OnkelMato.Thaddeus.Telegram.Requests;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 
 namespace OnkelMato.Thaddeus.Telegram;
 
-internal class BotMessageToRequestConverter : IBotMessageToRequestConverter
+public class BotMessageToRequestConverter : IBotMessageToRequestConverter
 {
     public RequestBase Convert(Message message, UpdateType type)
     {
         if (message.Text.StartsWith("Termine ", StringComparison.InvariantCultureIgnoreCase))
         {
-            var now = DateTime.Now;
-            var part = message.Text.Split(' ')[1];
-            return new GetAppointmentsRequest(message.Chat.Id, message.Chat.Id.ToString(), DateOnly.ParseExact(part, ["d.M.yyyy"]));
+            var date = ParseDate(message.Text.Split(' ')[1]);
+            if (date == null) // todo return result with error message
+                return new EmptyRequestBase(message.Chat.Id, message.Chat.Id.ToString());
+
+            return new GetAppointmentsRequest(message.Chat.Id, message.Chat.Id.ToString(), date.Value);
         }
 
         if (message.Text.StartsWith("Termin ", StringComparison.InvariantCultureIgnoreCase))
         {
-            var now = DateTime.Now;
             var part = message.Text.Split(' ',4, StringSplitOptions.TrimEntries);
-            var date = DateOnly.ParseExact(part[1], ["d.M.yyyy"]);
-            var time = TimeOnly.ParseExact(part[2], ["h:mm", "h"]);
+            if (part.Length < 4) // todo return result with error message
+                return new EmptyRequestBase(message.Chat.Id, message.Chat.Id.ToString());
+
+            if (string.IsNullOrWhiteSpace(part[3])) // todo return result with error message
+                return new EmptyRequestBase(message.Chat.Id, message.Chat.Id.ToString());
+
+            var date = ParseDate(part[1]);
+            if (date == null) // todo return result with error message
+                return new EmptyRequestBase(message.Chat.Id, message.Chat.Id.ToString());
+
+            var time = ParseTime(part[2]);
+            if (time == null) // todo return result with error message
+                return new EmptyRequestBase(message.Chat.Id, message.Chat.Id.ToString());
 
             return new AddAppointmentRequest(message.Chat.Id, message.Chat.Id.ToString(), new Appointment()
             {
-                Start = date.ToDateTime(time),
-                End = date.ToDateTime(time).AddMinutes(30),
+                Start = date.Value.ToDateTime(time.Value),
+                End = date.Value.ToDateTime(time.Value).AddMinutes(30),
                 Title = part[3]
             });
         }
 
         // make some factory here. or as extension method link message.GetRequestBase();
         return new EmptyRequestBase(message.Chat.Id, message.Chat.Id.ToString());
+    }
+
+    private TimeOnly? ParseTime(string time)
+    {
+        if (TimeOnly.TryParseExact(time, "H:mm", out var timeResult))
+            return timeResult;
+
+        var parts = time.Split(':', StringSplitOptions.RemoveEmptyEntries);
+        if (parts.Length == 1 && int.TryParse(parts[0], out var hour))
+        {
+            return new TimeOnly(hour, 0);
+        }
+
+        return null;
+    }
+
+    private DateOnly? ParseDate(string date)
+    {
+        if (DateOnly.TryParseExact(date, "d.M.yyyy", out var dteResult))
+            return dteResult;
+
+        var parts = date.Split('.', StringSplitOptions.RemoveEmptyEntries);
+        switch (parts.Length)
+        {
+            case 2:
+                if (int.TryParse(parts[0], out var day) && int.TryParse(parts[1], out var month))
+                {
+                    var now = DateTime.Now;
+                    return new DateOnly(now.Year, month, day);
+                }
+                break;
+            case 1:
+                if (int.TryParse(parts[0], out var dayOnly))
+                {
+                    var now = DateTime.Now;
+                    return new DateOnly(now.Year, now.Month, dayOnly);
+                }
+                break;
+        }
+
+        return null;
     }
 }
